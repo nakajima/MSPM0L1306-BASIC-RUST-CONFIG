33fb10bd0c1c89d1ca6c53b0ee02ba3ef98a852e
Pat Nakajima <patnakajima@gmail.com>
2025-03-11 20:42:43 -0700

    remove vs stuff

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,14 @@
 #![no_std]
 #![no_main]
 
-
 use panic_halt as _; // used to allow rust to run so it knows what to do if program crashes
 
+use cortex_m_rt::entry; // gives access to main function
 
-use cortex_m_rt::entry;// gives access to main function
-
+#[allow(unused_imports)]
 use MSPM0L1306_HAL;
 
-
-
 #[entry]
-fn main()->!{
-
-
-
-
-loop{
-
+fn main() -> ! {
+    loop {}
 }
-}
-
-
